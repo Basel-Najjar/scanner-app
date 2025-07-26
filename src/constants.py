fe02f669414a4f37d9072be9d52ae6fb2850f64f
Basel-Najjar <basel.s.najjar@gmail.com>
2025-07-26 14:44:04 -0500

    added bare bones TPO calculation in momentum.ipynb

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,2 @@
-default_lookback: int = 14  # default lookback window for sentiment classification
+default_lookback: int = 20  # default lookback window for sentiment classification
 base_url = "https://api.orats.io/datav2/"
